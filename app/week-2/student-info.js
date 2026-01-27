49e844b36043f80c052f5a40584fc6847abc89dd
J-Wales <151964658+J-Wales@users.noreply.github.com>
2026-01-27 12:15:05 -0700

    Week 2 assignment

---
+++ app/week-2/student-info.js
@@ -0,0 +1,7 @@
+import Link from "next/link";
+
+export function StudentInfo() {
+  return (
+    (<p>J Wales</p>), (<Link href="https://github.com/J-Wales">Github</Link>)
+  );
+}
---
+++ app/week-2/student-info.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export function StudentInfo() {
+  return (
+    <main>
+      <p>Name: J Wales</p>
+      <Link href="https://github.com/J-Wales">Github: https://github.com/J-Wales</Link>
+    </main>
+  );
+}
+
